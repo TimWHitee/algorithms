7d9d5f6241aca0fee7a4fae37c2f17e22796b535
Тимофей Морозов <tim7.moroz0v@yandex.ru>
2024-03-19 23:55:50 +0300

    Added not oriented graph finder

--- cycle_finder_not_oriented.py
+++ cycle_finder_not_oriented.py
@@ -1,1 +1,24 @@
-"to be done"
+used = []
+graph = {
+    1: [2, 3],
+    2: [1, 4],
+    3: [1, 5],
+    4: [2, 5],
+    5: [3, 4]
+}
+
+def dfs(node, parent):
+    used.append(node)
+    for nei in graph[node]:
+        if nei not in used:
+            dfs(nei, node)
+        elif  nei != parent:
+            return True
+    return False
+
+for node in graph:
+    if node not in used:
+        if dfs(node, None):
+            print("Cycle detected")
+            exit()
+print("No cycle detected")
